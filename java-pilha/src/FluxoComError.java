a156043aac94948dfad460cc056ce467e4b28f1d
Luís Eduardo Angelini Marquitti <luismarquitti@gmail.com>
2021-01-25 22:44:33 -0300

    Aula 04 do Curso Java Exceções

--- java-pilha/src/FluxoComError.java
+++ java-pilha/src/FluxoComError.java
@@ -1,10 +1,10 @@
-public class FluxoComTratamento {
+public class FluxoComError {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try{
             metodo1();
-        } catch(ArithmeticException | NullPointerException | MyException ex) {
+        } catch(ArithmeticException | NullPointerException ex) {
             String msg = ex.getMessage();
             System.out.println("Exception " + msg);
             ex.printStackTrace();
@@ -12,14 +12,15 @@ public class FluxoComTratamento {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MyException {
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MyException {
-        System.out.println("Ini do metodo2");
-        throw new MyException("Deu muito errado");
+    private static void metodo2() {
+        System.out.println("Ini do metodo 2");
+        metodo2();
+        System.out.println("Fim do metodo 2");
     }
 }
